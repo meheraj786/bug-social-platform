29bebd2128a09bfafa1630b0dacc80cc91e44ccd
meheraj786 <meherajhosen786@gmail.com>
2025-07-17 00:03:03 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React,{ useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Banner from "./components/banner/Banner";
+import BlogPostForm from "./components/blogPostForm/BlogPostForm";
+import BlogList from "./components/blogList/BlogList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <Navbar />
+      <Banner />
+      <BlogPostForm />
+      <BlogList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import Container from "../../layouts/Container";
+import Flex from "../../layouts/Flex";
+import { LuPenLine } from "react-icons/lu";
+import { RiHome2Line } from "react-icons/ri";
+import { CgNotes } from "react-icons/cg";
+import { FaRegQuestionCircle } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="bg-black font-primary text-white py-4">
+      <Container>
+        <Flex>
+          <div className="logo flex items-center gap-x-2 text-[20px] font-semibold">
+            <LuPenLine size={30} />
+            Anonymous Blog
+          </div>
+          <ul>
+            <Flex>
+              <li className="flex items-center gap-x-1 mx-5">
+                <RiHome2Line /> Home
+              </li>
+              <li className="flex items-center gap-x-1 mx-5">
+                <CgNotes />
+                Blogs
+              </li>
+              <li className="flex items-center gap-x-1 mx-5"><FaRegQuestionCircle />
+About</li>
+            </Flex>
+          </ul>
+        </Flex>
+      </Container>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
