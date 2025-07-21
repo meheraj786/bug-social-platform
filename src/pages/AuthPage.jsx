a4a003b809d1718cf72bcfd9609cb8475ca26fe3
meheraj786 <meherajhosen786@gmail.com>
2025-07-21 14:53:55 +0600

    password show icon add

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -16,14 +16,16 @@ import { useDispatch } from "react-redux";
 import { setUser } from "../features/user/userSlice";
 import { getDatabase, ref, set } from "firebase/database";
 import { MoonLoader } from "react-spinners";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const AuthPage = () => {
   const coudinaryApi = import.meta.env.VITE_CLOUDINARY_API;
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [isLoading, setIsLoading]= useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [imgLoading, setImgLoading] = useState(false);
   const [isLogin, setIsLogin] = useState(false);
+  const [showPass, setShowPass] = useState(false);
   const [signupInfo, setSignupInfo] = useState({
     name: "",
     email: "",
@@ -83,11 +85,18 @@ const AuthPage = () => {
   };
   const signupSubmitHandler = () => {
     const auth = getAuth();
-    setIsLoading(true)
+    setIsLoading(true);
 
-    if (!signupInfo.name || !signupInfo.email || !signupInfo.password || !signupInfo.imageUrl || !signupInfo.bio || !signupInfo.location) {
+    if (
+      !signupInfo.name ||
+      !signupInfo.email ||
+      !signupInfo.password ||
+      !signupInfo.imageUrl ||
+      !signupInfo.bio ||
+      !signupInfo.location
+    ) {
       toast.error("Please fill in all fields");
-      setIsLoading(false)
+      setIsLoading(false);
       return;
     }
 
@@ -123,20 +132,20 @@ const AuthPage = () => {
           bio: "",
         });
         setIsLogin(true);
-        setIsLoading(false)
+        setIsLoading(false);
       })
       .catch((error) => {
         toast.error(error.message);
-        setIsLoading(false)
+        setIsLoading(false);
       });
   };
 
   const loginSubmitHandler = () => {
-    setIsLoading(true)
+    setIsLoading(true);
     const auth = getAuth();
 
     if (!loginInfo.email || !loginInfo.password) {
-      setIsLoading(false)
+      setIsLoading(false);
       toast.error("Please enter email and password");
       return;
     }
@@ -157,11 +166,11 @@ const AuthPage = () => {
         setTimeout(() => {
           navigate("/");
         }, 2000);
-        setIsLoading(false)
+        setIsLoading(false);
       })
       .catch((error) => {
         toast.error(error.message);
-        setIsLoading(false)
+        setIsLoading(false);
       });
   };
 
@@ -185,18 +194,26 @@ const AuthPage = () => {
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
             />
             <label htmlFor="">Your Password</label>
+            <div className="relative">
             <input
               value={loginInfo.password}
               onChange={(e) => loginChangeHandler(e)}
-              type="text"
+              type={showPass ? "text":"password"}
               name="password"
               placeholder="Enter Your Password"
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
             />
-            {
-              isLoading ? <BeatLoader className="text-center mx-auto"/> : <Button onClick={loginSubmitHandler}>Log in</Button>
+                        {
+              showPass ? <FaRegEye onClick={()=>setShowPass(false)} className="absolute right-3 top-1/2 -translate-y-1/2" /> : <FaRegEyeSlash onClick={()=>setShowPass(true)} className="absolute right-3 top-1/2 -translate-y-1/2" /> 
             }
-            
+
+            </div>
+            {isLoading ? (
+              <BeatLoader className="text-center mx-auto" />
+            ) : (
+              <Button onClick={loginSubmitHandler}>Log in</Button>
+            )}
+
             <p className="text-center text-medium">
               Don't have Account?{" "}
               <span
@@ -249,7 +266,6 @@ const AuthPage = () => {
             </div>
             <div className="flex flex-col items-center text-center">
               <div className="w-28 h-28 rounded-full bg-gray-300 mb-2 overflow-hidden flex justify-center items-center relative border-2 border-black">
-                
                 {imgLoading ? (
                   <MoonLoader />
                 ) : (
@@ -274,15 +290,15 @@ const AuthPage = () => {
                     className="hidden"
                     required
                   />
-                  
                 </div>
-                
               </div>
-              <p className="text-red-600 text-[14px] my-3">*You can not change those info later</p>
+              <p className="text-red-600 text-[14px] my-3">
+                *You can not change those info later
+              </p>
             </div>
             <label htmlFor="">Your Name</label>
             <input
-            required
+              required
               value={signupInfo.name}
               onChange={(e) => signupChangeHandler(e)}
               type="text"
@@ -292,7 +308,7 @@ const AuthPage = () => {
             />
             <label htmlFor="">Your Email</label>
             <input
-            required
+              required
               value={signupInfo.email}
               onChange={(e) => signupChangeHandler(e)}
               type="text"
@@ -301,15 +317,24 @@ const AuthPage = () => {
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
             />
             <label htmlFor="">Your Password</label>
+            <div className="relative">
             <input
-            required
+              required
               value={signupInfo.password}
               onChange={(e) => signupChangeHandler(e)}
-              type="text"
+              type={showPass ? "text" : "password"}
               name="password"
               placeholder="Enter Your Password"
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
             />
+            {
+              showPass ? <FaRegEye onClick={()=>setShowPass(false)} className="absolute right-3 top-1/2 -translate-y-1/2" /> : <FaRegEyeSlash onClick={()=>setShowPass(true)} className="absolute right-3 top-1/2 -translate-y-1/2" /> 
+            }
+              
+              
+
+
+            </div>
             <label htmlFor="">Your Phone</label>
             <input
               value={signupInfo.phone}
@@ -321,7 +346,7 @@ const AuthPage = () => {
             />
             <label htmlFor="">Your Location</label>
             <input
-            required
+              required
               value={signupInfo.location}
               onChange={(e) => signupChangeHandler(e)}
               type="text"
@@ -331,7 +356,7 @@ const AuthPage = () => {
             />
             <label htmlFor="">Your Bio</label>
             <input
-            required
+              required
               value={signupInfo.bio}
               onChange={(e) => signupChangeHandler(e)}
               type="text"
@@ -339,9 +364,11 @@ const AuthPage = () => {
               placeholder="Enter Your Short Bio"
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
             />
-            {
-              isLoading ? <BeatLoader className="text-center mx-auto"/> :  <Button onClick={signupSubmitHandler}>Sign Up</Button>
-            }
+            {isLoading ? (
+              <BeatLoader className="text-center mx-auto" />
+            ) : (
+              <Button onClick={signupSubmitHandler}>Sign Up</Button>
+            )}
             <p className="text-center text-medium">
               Already Have Account?{" "}
               <span
